b806c068810976aad32a93410b2a288c6032c204
ASCHORTGEN <alexandre.schortgen@grand-insep.fr>
2022-11-22 09:52:15 +0100

    quick fix

--- code/helper_functions.py
+++ code/helper_functions.py
@@ -56,13 +56,20 @@ def calculate_Oline_zones(points, line_of_scrimmage):
     hull = ConvexHull(init_zone_points[:, :2])
     return init_zone_points[hull.vertices,:2]
 
+# ------------------------------------------------- #
+#          Calcul Orientations et face à face       #
+# ------------------------------------------------- #
+
 def compute_orientation(data):
     copy = data.copy()
     copy = copy.assign(o_x = np.sin(copy.o*2*np.pi/360))
     copy = copy.assign(o_y = np.cos(copy.o*2*np.pi/360))
     return copy
 
-def face2face(tracking_data, scouting_data):
+
+def face2face(gameId, playId, tracking_data, scouting_data):
+    tracking_data = tracking_data.query(f"gameId == {gameId} & playId == {playId}")
+    scouting_data = scouting_data.query(f"gameId == {gameId} & playId == {playId}")
     """
     run in a for loop to compute for each play at each frame
     """
@@ -72,7 +79,7 @@ def face2face(tracking_data, scouting_data):
     opp_orientation = pd.merge(tracking_data[["nflId","o_x","o_y"]],blocked_opponent,how="inner",left_on="nflId", right_on="pff_nflIdBlockedPlayer")
     opp_orientation = opp_orientation.rename(columns={"nflId_y" : "nflId"})
     #print(opp_orientation)
-    data_with_opp_orientation = pd.merge(tracking_data, opp_orientation[["nflId","o_x","o_y","pff_nflIdBlockedPlayer"]],how="inner",on="nflId")
+    data_with_opp_orientation = pd.merge(tracking_data , opp_orientation[["nflId","o_x","o_y","pff_nflIdBlockedPlayer"]],how="inner",on="nflId")
     data_with_opp_orientation = data_with_opp_orientation.rename(columns={"o_x_x" : "o_x_off", 
                                                                         "o_y_x" : "o_y_off",
                                                                         "o_x_y" : "o_x_def",
@@ -80,7 +87,10 @@ def face2face(tracking_data, scouting_data):
 
     data_with_opp_orientation["f2f"] = data_with_opp_orientation.o_x_off*data_with_opp_orientation.o_x_def < 0
     return data_with_opp_orientation
+    
 
+
+    
 
 
 
